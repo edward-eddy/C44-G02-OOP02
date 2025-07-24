38c152e080ebe4722323a3ef4cb37763e85d2b6c
edwar <edwardsameh99@gmail.com>
2025-07-24 19:38:41 +0300

    Vid01

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Class VS. Struct
+            // Class ---------------------- VS. ------------------> Struct
+
+            // Reference Type ------------------------------------> Value Type
+            // Uses all 4 Pillers of OOP -------------------------> Uses All But Inheritance
+            // Have a default constructor ------------------------> structs do not have a default constructor
+            // new Keyword creates the object --------------------> Choose the Constructor
+            // Has a Default Constructor -------------------------> The default constructor is called implecetly by the compiler 
+            // Default constructor does nothing ------------------> Initialize the attributes and assigns the Default Values
+            // User Defined Constructor replaces the default -----> Only The Parameterless replaces the default
+            // ??????? You cant def
+            // All Six Access Modifires --------------------------> Private, Internal, Public
+            #endregion
+
+
+
         }
     }
 }
