b998dfb256e3081621e1df73aa7d17195aa4b392
edwar <edwardsameh99@gmail.com>
2025-07-27 14:12:29 +0300

    Task02

---
+++ Task02/Class02.cs
@@ -0,0 +1,25 @@
+﻿using ClassLibrary1;
+
+namespace Task02
+{
+    // Different Assembly, Derived Class
+    // "public" is accessable
+    // "protected" and "internal protected" are accessable
+    internal class Class02 : ClassLib01
+    {
+        public void Fun02()
+        {
+            Class02 class02 = new Class02();
+
+            class02.Public();
+            //class02.Private();
+            //class02.Internal();
+
+            class02.JustProtected();
+            //class02.PrivateProtected();
+            class02.InternalProtected();
+
+
+        }
+    }
+}
---
+++ Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿using Task02.Classes;
+
+namespace Task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class01 class01 = new Class01();
+            Class02 class02 = new Class02();
+
+            class01.InternalProtected = 13;
+            class02.InternalProtected = 12;
+
+        }
+    }
+}
